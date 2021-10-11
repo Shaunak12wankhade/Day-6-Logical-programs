14d3b36f436545784fb0e97a0d3d89697b931146
unknown <shaunak12wankhade@ggmail.com>
2021-10-11 14:39:06 +0530

    [Shaunak] Added : Reverse number problem

---
+++ Reverse number.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_programs
+{
+    class Reverse_number
+    {
+        static void Main(string[] args)
+        {
+            int Num;
+            Console.Write("Enter the Number:  ");
+            Num = int.Parse(Console.ReadLine());
+
+
+
+            int reverse = 0;
+
+            while (Num > 0)
+            {
+                int Output = (Num % 10);
+                reverse = reverse * 10 + Output;
+                Num = (Num / 10);
+
+
+            }
+
+
+            Console.WriteLine(+reverse + ":is a reverse of a number");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
