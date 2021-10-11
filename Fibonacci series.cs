f712d95416e85020ec703e23e40411f431f23f7d
unknown <shaunak12wankhade@ggmail.com>
2021-10-11 13:57:28 +0530

    [Shaunak] Added : Fibonacci series problem

---
+++ Fibonacci series.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_programs
+{
+    class Fibonacci_series
+    {
+        static void Main(string[] args)
+        {
+            int Num;
+
+            Console.Write("Enter the number of elements :    ");
+            Num = int.Parse(Console.ReadLine());
+
+
+
+            int f1 = 0, f2 = 1;
+            int fSeries;
+            int i;
+            Console.WriteLine("Fibonacci sequence : " + f1);
+            Console.WriteLine("Fibonacci sequence : " + f2);
+            for (i = 2; i < Num; i++)
+            {
+                fSeries = f1 + f2;
+                Console.WriteLine("Fibonacci sequence : " + fSeries);
+                f1 = f2;
+                f2 = fSeries;
+
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
