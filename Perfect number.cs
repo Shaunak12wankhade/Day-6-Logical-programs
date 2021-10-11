16196abb543f7bc694d4b2eb49fe46eec1491124
unknown <shaunak12wankhade@ggmail.com>
2021-10-11 14:29:02 +0530

    [Shaunak] Added : Perfect number problem

---
+++ Perfect number.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_programs
+{
+    class Perfect_number
+    {
+        static void Main(string[] args)
+        {
+           
+
+                int Num;
+                Console.Write("Enter the Number : ");
+                Num = int.Parse(Console.ReadLine());
+
+                int i = 1;
+                int sum = 0;
+
+                while (i <= Num / 2)
+                {
+                    int Output = (Num % i);
+                    if (Output == 0)
+                    {
+                        sum = sum + i;
+
+
+
+                    }
+                    i++;
+                }
+                if (Num == sum)
+                {
+                    Console.WriteLine(+Num + ":is a perfect Number");
+
+                }
+                else
+                {
+                    Console.WriteLine(+Num + ":is not a perfect Number");
+
+                }
+
+
+                Console.ReadLine();
+
+
+            
+        }
+    }
+}
